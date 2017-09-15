527dabea7b4f5d17f2561b8b01ffde67ad24505d
Adrian Statescu <mergesortv@gmail.com>
2017-09-14 18:03:26 -0700

    euclidean distance in Golang

---
+++ euclidean_distance.go
@@ -0,0 +1,30 @@
+package main
+
+import ("fmt"
+        "math") 
+
+func euclidean_distance_procedural(a []int, b []int) float64 {
+
+      var n, s int
+
+          n = len(a)
+
+          s = 0
+
+     for i := 0; i < n; i +=1 {
+ 
+         s += (b[i]-a[i])*(b[i]-a[i])
+     }
+
+     f := float64( s )
+
+     return math.Sqrt( f )
+}
+
+func main() {
+
+     var a = []int{1,2,3}
+        b := []int{4,5,6}
+
+     fmt.Printf("Euclidean Distance -> %f", euclidean_distance_procedural(a,b))
+}
