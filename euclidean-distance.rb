3fcae5c127bfe51ebb970e6dcef41dda54939771
Adrian Statescu <mergesortv@gmail.com>
2017-09-14 17:25:06 -0700

    procedural solution

--- euclidean-distance.rb
+++ euclidean-distance.rb
@@ -6,11 +6,23 @@ def euclidean_distance(p, q)
 
 end
 
+def euclidean_distance_procedural(p, q)
+
+    s = 0
+    n = p.length - 1
+    for i in 0..n
+        s += (q[i]-p[i])**2 
+    end 
+    Math.sqrt(s) 
+end
+
+
 class TestSimpleNumber < Test::Unit::TestCase
  
   def test_simple
            
       assert_equal(5.196152422706632, euclidean_distance([1,2,3],[4,5,6]))
+      assert_equal(5.196152422706632, euclidean_distance_procedural([1,2,3],[4,5,6]))
 
   end
  
